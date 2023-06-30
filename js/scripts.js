01ddd8d7ba569ede247dae39ee0db1a78f329b3e
Patrick Dolan <dolanp1992@gmail.com>
2023-06-30 10:48:33 -0700

    change pigLatin to move consonants to end of word and add ay

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 function pigLatin(userPhrase) {
   const vowels = ["a", "e", "i", "o", "u"];
-  const phraseWithoutPunctuation = userPhrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  const phraseWithoutPunctuation = userPhrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").trim();
   const phraseArr = phraseWithoutPunctuation.split(" ");
   const pigLatinArr = [];
   phraseArr.forEach((word) => {
+    // Solve for all words that start with a vowel
     if (vowels.includes(word[0])) {
       pigLatinArr.push(word + "way");
     } else {
-      pigLatinArr.push(word);
+      // Solve for consonants rule
+      let wordArr = word.split("");
+      let consonants = "";
+      let firstVowelIndex = 0;
+      for (let i = 0; i < wordArr.length; i++) {
+        if (!vowels.includes(wordArr[i])) {
+          consonants += wordArr[i];
+        } else {
+          firstVowelIndex = i;
+          break;
+        }
+      }
+      pigLatinArr.push(word.slice(firstVowelIndex, word.length) + consonants + "ay");
     }
   })
   return pigLatinArr.join(" ");
@@ -16,7 +29,7 @@ function pigLatin(userPhrase) {
 window.addEventListener("load", () => {
   document.getElementById("phrase-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    const phraseToTranslate = document.querySelector("#userPhrase").value;
+    const phraseToTranslate = document.querySelector("#user-phrase").value;
     document.querySelector("#pig-latin-display").innerText = pigLatin(phraseToTranslate);
     document.querySelector("#user-phrase").value = "";
   })
